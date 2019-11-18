01845548c08e8be72067dd4f4be4af83ae7c4219
ayush2297 <ayush.saraf47@gmail.com>
2019-11-18 10:27:10 +0530

    added: a function to reset the board to initial values

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,1 +1,21 @@
+#! /bin/bash -x
+
 echo "tic-tac-toe game*************"
+#constants
+declare MAX_CELLS_AVAILABLE=9
+
+#arrays and dictionaries
+declare -a ticTacToeBoard
+
+function reset_the_board(){
+	for (( i=1 ; i<=MAX_CELLS_AVAILABLE ; i++ ))
+	do
+		ticTacToeBoard[$i]="$i"
+	done
+}
+
+function the_main_exec_starts_here(){
+	reset_the_board
+}
+
+the_main_exec_starts_here
