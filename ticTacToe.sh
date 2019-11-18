14bcb3e0bae0bba636ef52717828d6302c1f0e07
ayush2297 <ayush.saraf47@gmail.com>
2019-11-18 14:58:27 +0530

    added: win checking for diagonals working

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -49,25 +49,85 @@ function user_chance(){
 	fi
 	if [[ ${ticTacToeBoard[$chosenCell]} =~ [0-9] ]]
 	then
-		ticTacToeBoard[$row]=$PLAYER_SYMBOL
+		ticTacToeBoard[$chosenCell]=$PLAYER_SYMBOL
 	else
 		echo "cell already occupied.... try another cell"
 		user_chance
 	fi
 }
 
+function check_diagonals_for_win(){
+	local mainDiagWinning=1
+	#checking main diagonal
+	for (( i=1 ; $i<$MAX_CELLS_AVAILABLE ; i=$(($i+$rowColCount+1)) ))
+	do
+		if [ ${ticTacToeBoard[$i]} == ${ticTacToeBoard[$(($i+$rowColCount+1))]} ]
+		then
+			mainDiagWinning=$(($mainDiagWinning+1))
+		else
+			break
+		fi
+	done
+	if [[ $mainDiagWinning -eq $rowColCount ]] && [[ ${ticTacToeBoard[1]} == $1 ]]
+	then
+		echo 1
+		return
+	fi
+	#checking for reverse diagonal
+	local revDiagWinning=1
+	for (( i=$rowColCount ; $i<$MAX_CELLS_AVAILABLE ; i=$(($i+$rowColCount-1)) ))
+	do
+		if [ ${ticTacToeBoard[$i]} == ${ticTacToeBoard[$(($i+$rowColCount-1))]} ]
+		then
+			revDiagWinning=$(($revDiagWinning+1))
+		else
+			break
+		fi
+	done
+	if [[ $revDiagWinning -eq $rowColCount ]] && [[ ${ticTacToeBoard[$rowColCount]} == $1 ]]
+	then
+		echo 1
+		return
+	fi
+	#if no winning condition then ....
+	echo 0
+}
+
+function check_if_this_player_won(){
+	local thisPlayer=$1
+	local winCounter=0
+	local thisPlayerSymbol=0
+	if [ $thisPlayer == "user" ]
+	then
+		thisPlayerSymbol=$PLAYER_SYMBOL
+	else
+		thisPlayerSymbol=$COMPUTER_SYMBOL
+	fi
+	winCounter=$(($winCounter+ $( check_diagonals_for_win $thisPlayerSymbol ) ))
+	echo $winCounter
+}
+
 function the_main_exec_starts_here(){
 	local whoseChanceIsIt=0
-	local switchCounter=0
+	local weHaveAWinner=0
 	reset_the_board
-	whoseChanceIsIt=$( toss_to_decide_who_plays_first )
-	if [ $whoseChanceIsIt == "user" ]
-	then
-		display_the_board
-		user_chance
-	else
-		echo "comp plays"
-	fi
+	local whoseChanceIsIt=$( toss_to_decide_who_plays_first )
+	chanceNumber=1
+	while [ $chanceNumber -le $MAX_CELLS_AVAILABLE ]
+	do
+		if [ $whoseChanceIsIt == "user" ]
+		then
+			#display_the_board
+			user_chance
+			weHaveAWinner=$( check_if_this_player_won $whoseChanceIsIt )
+		else
+			echo "comp plays"
+		fi
+		if [ $weHaveAWinner -gt 0 ]
+		then
+			break
+		fi
+	done
 }
 
 the_main_exec_starts_here
